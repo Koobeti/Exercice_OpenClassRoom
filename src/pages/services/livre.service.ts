72e2c4d50a2d6a32d2d62aeb965d6df1ac7fe13d
Douzet Nicolas <shangdi@hotmail.fr>
2019-09-16 15:44:25 +0200

    Ajout des boutons "prêter" et "rendre"

--- src/pages/services/livre.service.ts
+++ src/pages/services/livre.service.ts
@@ -1,8 +1,16 @@
 export class LivresService {
 
   listLivre = [
-    {auteur: "Agatha Christie", titre: "Dix petits nègres", description: "Dans le roman, dix personnages, qui ont tous, par le passé, perpétré un homicide devant lequel la justice est impuissante, sont invités à se rendre sur une île : bien qu'ils en soient alors les seuls résidents, ils sont mystérieusement assassinés les uns après les autres, d'une façon qui rappelle les dix couplets d'une comptine."},
-    {auteur: "Victor Hugo", titre: "Les misérables", description: "Dans ce roman emblématique de la littérature française qui décrit la vie de pauvres gens dans Paris et la France provinciale du xixe siècle, l'auteur s'attache plus particulièrement au destin du bagnard Jean Valjean. C'est un roman historique, social et philosophique dans lequel on retrouve les idéaux du romantisme et ceux de Victor Hugo concernant la nature humaine."}
+    { auteur: "Agatha Christie",
+      titre: "Dix petits nègres",
+      description: "Dans le roman, dix personnages, qui ont tous, par le passé, perpétré un homicide devant lequel la justice est impuissante, sont invités à se rendre sur une île : bien qu'ils en soient alors les seuls résidents, ils sont mystérieusement assassinés les uns après les autres, d'une façon qui rappelle les dix couplets d'une comptine.",
+      preter: 1
+    },
+    { auteur: "Victor Hugo", 
+      titre: "Les misérables",
+      description: "Dans ce roman emblématique de la littérature française qui décrit la vie de pauvres gens dans Paris et la France provinciale du xixe siècle, l'auteur s'attache plus particulièrement au destin du bagnard Jean Valjean. C'est un roman historique, social et philosophique dans lequel on retrouve les idéaux du romantisme et ceux de Victor Hugo concernant la nature humaine.",
+      preter: 0
+    }
   ];
 
 }
